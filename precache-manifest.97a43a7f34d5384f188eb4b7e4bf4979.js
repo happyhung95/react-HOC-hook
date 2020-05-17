a225c1443f1d485856e66d7e2ca20d130a31a581
Hung Nguyen <happyhung@yahoo.com>
2020-05-17 23:05:56 +0300

    Updates

---
+++ precache-manifest.97a43a7f34d5384f188eb4b7e4bf4979.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a41b691d1966a38ed423d8db74a72fb",
+    "url": "/ft4-react-assignment-2/index.html"
+  },
+  {
+    "revision": "2349f706b653c6df5bf8",
+    "url": "/ft4-react-assignment-2/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "0d559c610a8676f3f583",
+    "url": "/ft4-react-assignment-2/static/js/2.2ee06dd7.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/ft4-react-assignment-2/static/js/2.2ee06dd7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2349f706b653c6df5bf8",
+    "url": "/ft4-react-assignment-2/static/js/main.c38ae1a7.chunk.js"
+  },
+  {
+    "revision": "96bfce40569ad606c08d",
+    "url": "/ft4-react-assignment-2/static/js/runtime-main.81ef854d.js"
+  }
+]);
